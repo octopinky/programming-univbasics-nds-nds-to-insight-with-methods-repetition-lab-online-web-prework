151d350ca83138823a022f79f056f5a0ff97baa5
Olga  Chupp <olga.chupp@gmail.com>
2019-10-27 23:01:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  binding.pry
+  #binding.pry
   director_totals = directors_totals(source)
   director_names = list_of_directors(source)
 end
